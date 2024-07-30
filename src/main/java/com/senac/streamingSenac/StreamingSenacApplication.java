8a5032d0747dfa1a45d779d974ab0f00962c5915
Thiago de Sá <thiagosafreitas@hotmail.com>
2024-07-30 13:26:34 -0300

    projeto spring boot

---
+++ src/main/java/com/senac/streamingSenac/StreamingSenacApplication.java
@@ -0,0 +1,13 @@
+package com.senac.streamingSenac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamingSenacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamingSenacApplication.class, args);
+	}
+
+}
