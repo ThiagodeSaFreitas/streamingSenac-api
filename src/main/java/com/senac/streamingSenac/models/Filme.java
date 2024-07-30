bb12910b0debcff9eec52479e5220fa0f09af7b0
Thiago de Sá <thiagosafreitas@hotmail.com>
2024-07-30 15:03:57 -0300

    criando repository

--- src/main/java/com/senac/streamingSenac/models/Filme.java
+++ src/main/java/com/senac/streamingSenac/models/Filme.java
@@ -3,11 +3,44 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.senac.streamingSenac.models;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-/**
- *
- * @author Thiago de Sá
- */
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "filme")
 public class Filme {
+    
+    @OneToMany(mappedBy = "filme")
+    private List<Analise> analises = new ArrayList<Analise>();
+    
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(name = "titulo", nullable = false, unique = true)
+    private String titulo;
+    
+    @Column(name = "sinopse")
+    private String sinopse;
+    
+    @Column(name = "genero", nullable = false)
+    private String genero;
+    
+    @Column(name = "data_lançamento")
+    private String lancamento;
     
 }
